020cd7402f803a474776e23ece11b3a5b51db85f
Omar Madrigal <80352580+predye28@users.noreply.github.com>
2024-06-17 02:53:38 -0600

    actualizar actividades 2

--- backend/actualizarActividades.js
+++ backend/actualizarActividades.js
@@ -1,7 +1,7 @@
 const Actividad = require('./models/actividadModel');
 const Notificacion = require('./models/notificacionModel'); 
-const { PublishVisitor, ReminderVisitor } = require('./visitor');
-const { NotificationCenter } = require('./Observer'); // Importamos NotificationCenter
+//const { PublishVisitor, ReminderVisitor } = require('./visitor');
+//const { NotificationCenter } = require('./Observer'); // Importamos NotificationCenter
 let fechaSimulada;
 
 const enviarNotificacion = async (actividad, tipo) => {
@@ -35,24 +35,28 @@ const setFechaSimulada = async () => {
 };
 
 
-const notificationCenter = new NotificationCenter();
-class NotificacionObserver extends Observer {
-    update(actividad, tipo) {
-        enviarNotificacion(actividad, tipo); 
-    }
-}
+//const notificationCenter = new NotificationCenter();
+//class NotificacionObserver extends Observer {
+    //update(actividad, tipo) {
+        //enviarNotificacion(actividad, tipo); // Llama a la función de enviarNotificacion
+    //}
+//}
 
-const notificacionObserver = new NotificacionObserver();
 
-notificationCenter.attach(notificacionObserver);
+//const notificacionObserver = new NotificacionObserver();
+
+
+//notificationCenter.attach(notificacionObserver);
+
+
 
 const actualizarEstadoActividades = async () => {
     try {
         const actividades = await Actividad.find();
         const ahora = fechaSimulada || new Date();
 
-        const publishVisitor = new PublishVisitor();
-        const reminderVisitor = new ReminderVisitor();
+        //const publishVisitor = new PublishVisitor();
+        //const reminderVisitor = new ReminderVisitor();
 
         for (let actividad of actividades) {
             const { fechaPublicacion, fechasRecordatorio, estadoActividad, notificado, recordatoriosEnviados } = actividad;
@@ -63,7 +67,7 @@ const actualizarEstadoActividades = async () => {
                 actividad.notificado = true;
                 await actividad.save();
                 await enviarNotificacion(actividad, 'Actividad Publicada');
-                NotificationCenter.notify(actividad, 'Actividad Publicada'); 
+                //NotificationCenter.notify(actividad, 'Actividad Publicada'); // Notificar al NotificationCenter
             }
 
             // VERIFICAR PARA HACER EL RECORDATORIO
@@ -75,11 +79,11 @@ const actualizarEstadoActividades = async () => {
                     actividad.recordatoriosEnviados.push(fechaRecordatorio);
                     await actividad.save();
                     await enviarNotificacion(actividad, 'Recordatorio de Actividad');
-                    NotificationCenter.notify(actividad, 'Recordatorio de Actividad'); 
+                    //NotificationCenter.notify(actividad, 'Recordatorio de Actividad'); // Notificar al NotificationCenter
                 }
             }
-            actividad.accept(publishVisitor);
-            actividad.accept(reminderVisitor);
+            //actividad.accept(publishVisitor); // Publicar actividad
+            //actividad.accept(reminderVisitor); // Enviar recordatorio
         }
     } catch (error) {
         console.error('Error actualizando actividades:', error);
